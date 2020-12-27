e90057facd7972204d321e4b035f4bf3d34e7010
xippoGmbH <aurelio.gisler@xippo.ch>
2020-12-27 16:40:01 +0100

    test

--- src/EventListener/AhsFormDataListener.php
+++ src/EventListener/AhsFormDataListener.php
@@ -10,21 +10,31 @@
  * @see        https://github.com/xippoGmbH/contao-ahsform-bundle
  *
  */
- namespace XippoGmbH\ContaoAhsFormBundle\EventListener;
+namespace XippoGmbH\ContaoAhsFormBundle\EventListener;
 
 use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\Backend;
 use Contao\Form;
 
 /**
  * @Hook("processFormData")
  */
-class AhsFormDataListener
+class AhsFormDataListener extends Backend
 {
     public function __invoke(array $submittedData, array $formData, ?array $files, array $labels, Form $form): void
     {
         \System::getContainer()
 			->get('monolog.logger.contao')
 			->log(LogLevel::INFO, 'Ein Log-Eintrag', array(
+			'contao' => new ContaoContext(__CLASS__.'::'.__FUNCTION__, TL_GENERAL
+			)));
+    }
+
+    public function ahsForm($arrPost, $arrForm, $arrFiles)
+    {
+        \System::getContainer()
+			->get('monolog.logger.contao')
+			->log(LogLevel::INFO, 'Ein Log-Eintrag 2', array(
 			'contao' => new ContaoContext(__CLASS__.'::'.__FUNCTION__, TL_GENERAL
 			)));
     }
